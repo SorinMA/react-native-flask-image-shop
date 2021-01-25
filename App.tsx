5a8f73c10a8203c069bf37340ce794d5110cf2c7
SorinMA <sorinmartinescu9dev@gmail.com>
2021-01-25 20:08:52 +0200

    api + mobile image reciver: need to do check why the images doesn t load on web emualtor

--- App.tsx
+++ App.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const [images, setImages] = useState([]);
+  const [imagesLoaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    axios.get("http://192.168.0.33:5000/get-images")
+      .then((response) => {
+        setImages(response.data.allImages);
+        setLoaded(response.data.images);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const renderLogo = () => {
+    let images_to_show = null;
+    if (imagesLoaded === true) {
+      images_to_show = (Array.from(images))
+                             .map((data,i) => {
+                              return <Image key = {i} 
+                                            source = {{uri: `data:image/png;base64,${data}`}}
+                                            style={styles.logo}
+                                            resizeMode={'cover'}/>;
+                             });                
+    }
+
+    return images_to_show;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        {renderLogo()}
+        <Image style={styles.logo} source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"}} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 10,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  logo: {
+    width: 250,
+    height: 200,
   },
 });
+
