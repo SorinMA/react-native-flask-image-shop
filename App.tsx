2e13f39cba9daf2a88213e7c5216ded02a2be02b
SorinMA <sorinmartinescu9dev@gmail.com>
2021-01-26 14:31:30 +0200

    fix the issue

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Platform  } from 'react-native';
 import axios from 'axios';
 
 export default function App() {
@@ -16,16 +16,27 @@ export default function App() {
   }, []);
 
   const renderLogo = () => {
-    let images_to_show = null;
+    let images_to_show = (<></>);
     if (imagesLoaded === true) {
       images_to_show = (Array.from(images))
                              .map((data,i) => {
-                              return <Image key = {i} 
-                                            source = {{uri: `data:image/png;base64,${data}`}}
-                                            style={styles.logo}
-                                            resizeMode={'cover'}/>;
+                              if (data === undefined)
+                                return (<></>);
+                              if (Platform.OS === 'ios' || Platform.OS === 'android')
+                                return <Image key = {i}
+                                              source = {{uri: `data:image/jpeg;base64,${data}`}}
+                                              style = {styles.logo}
+                                              />
+                              return (      
+                                              <img key = {i} 
+                                              src = {`data:image/jpeg;base64,${data}`}
+                                              />
+                                             
+                                            
+                                            );
                              });                
     }
+
 
     return images_to_show;
   }
@@ -33,7 +44,6 @@ export default function App() {
     <>
       <View style={styles.container}>
         {renderLogo()}
-        <Image style={styles.logo} source={{uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"}} />
       </View>
     </>
   );
